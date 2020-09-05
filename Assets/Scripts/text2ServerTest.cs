4c73c96d8fa7b2ae959ed07cb126c907174483ca
ReidulousKane <reidulouskane@gmail.com>
2020-09-06 01:23:15 +1000

    php figuring out
    found out how to communicate between unity and database using php and unitywebrequests

--- Assets/Scripts/text2ServerTest.cs
+++ Assets/Scripts/text2ServerTest.cs
@@ -1,33 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 using TMPro;
 
 public class text2ServerTest : MonoBehaviour
 {
     private bool enterUp = true;
+    Transform canvas;
+    Transform testInputFieldTransform;
+    TMP_InputField testInputField;
+    // Button RegisterButton;
+    
     // Start is called before the first frame update
     void Start()
     {
-    	this.gameObject.transform.Find( "Text (TMP)" ).GetComponent<TextMeshProUGUI>().SetText( "Loading..." );        
+    	canvas = this.gameObject.transform;
+    	canvas.Find( "Text (TMP)" ).GetComponent<TextMeshProUGUI>().SetText( "Loading..." );
+    	testInputField = canvas.Find("testInputField").GetComponent<TMP_InputField>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( Input.GetKeyDown(KeyCode.Return) && enterUp && this.gameObject.transform.GetChild(0).GetChild(0).Find( "Text" ).GetComponent<TextMeshProUGUI>().text != "" )
+    	if( testInputField.isFocused )
     	{
-    		enterUp = false;
-    		Debug.Log( "Detected key down: Return" );
-    		string input = this.gameObject.transform.GetChild(0).GetChild(0).Find( "Text" ).GetComponent<TextMeshProUGUI>().text;
-    		Debug.Log( input );
-    		StartCoroutine(testServerConnect.PostTestStr( input ));
-    	}
-    	if ( Input.GetKeyDown(KeyCode.Return) && !enterUp )
-    	{
-    		enterUp = true;
-    		Debug.Log( "Detected key up: Return" );
-    		StartCoroutine(testServerConnect.GetTestStr( this.gameObject.transform.Find( "Text (TMP)" )/*.GetComponent<TextMeshProUGUI>()*/));
-    	} 
+			if ( Input.GetKeyDown(KeyCode.Return) && enterUp && testInputField.text != "" )
+	    	{
+	    		string input = testInputField.text;
+	    		enterUp = false;
+	    		Debug.Log( "Detected key down: Return" );
+	    		Debug.Log( input );
+	    		StartCoroutine(testServerConnect.PostTestStr( input ));
+	    	}
+	    	if ( Input.GetKeyDown(KeyCode.Return) && !enterUp )
+	    	{
+	    		enterUp = true;
+	    		Debug.Log( "Detected key up: Return" );
+	    		StartCoroutine(testServerConnect.GetTestStr( this.gameObject.transform.Find( "Text (TMP)" )));
+	    	}
+	   	}
+    }
+
+    public void RegisterButtonClicked()
+    {
+    	// about to test sending all this to localhost
+    	string usernameText = canvas.Find( "UserNameInput" ).GetComponent<TMP_InputField>().text;
+    	string emailText = canvas.Find( "EmailInput" ).GetComponent<TMP_InputField>().text;
+    	string PWText = canvas.Find( "PWInput" ).GetComponent<TMP_InputField>().text;
+    	string confirmPWText = canvas.Find( "PWConfirm" ).GetComponent<TMP_InputField>().text;
+    	Debug.Log( usernameText + " " + emailText + " " + PWText + " " + confirmPWText );
     }
 }
--- ServerSide/testReturn.php
+++ ServerSide/testReturn.php
@@ -1,12 +1,9 @@
 <?php
-    // Send variables for the MySQL database class.
 	$db = mysqli_connect("localhost", "root", "root") or die("Could not connect: " . mysqli_error()); 
     mysqli_select_db($db, "test") or die("Could not select database");
  
     $query = "SELECT `testString` FROM `testConnection` ORDER by `id` DESC LIMIT 1";
-    $result = mysqli_query($db, $query) or die("Query failed: " . mysql_error());
+    $sqlResult = mysqli_query($db, $query) or die("Query failed: " . mysql_error());
+    $result = mysqli_fetch_row($sqlResult);
 
-    // this is not yet working, yay for learning new stuff!
-    $row = mysqli_use_result($db);
- 
-    echo "Server sent: " . $row["testString"];
+    echo "Server returned: $result[0]";
